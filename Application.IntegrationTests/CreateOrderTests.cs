7dbb8b98fbbb70d82ab555b1f1b99baef9fc67ad
Michał Sykutera <msykutera@gmail.com>
2023-07-07 15:38:00 +0200

    Add CreateOrder tests

---
+++ Application.IntegrationTests/CreateOrderTests.cs
@@ -0,0 +1,20 @@
+using Application.CreateOrder;
+using FluentAssertions;
+
+namespace Application.IntegrationTests
+{
+    [TestFixture]
+    public class CreateOrderTests : Testing
+    {
+        [Test]
+        public async Task CreateOrderShouldSucceed()
+        {
+            var client = Harness.GetRequestClient<CreateOrderCommand>();
+            var command = new CreateOrderCommand(new List<int> { 1, 2 });
+            var response = await client.GetResponse<CreateOrderResult>(command);
+
+            response.Message.Success.Should().BeTrue();
+            response.Message.Id.Should().BeGreaterThan(0);
+        }
+    }
+}
---
+++ Application.IntegrationTests/Testing.cs
@@ -0,0 +1,46 @@
+﻿using Application.Common;
+using Infrastructure;
+using Infrastructure.Persistence;
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application.IntegrationTests;
+
+[SetUpFixture]
+public partial class Testing
+{
+    private ServiceProvider _provider;
+
+    public ITestHarness Harness = null!;
+
+    [OneTimeSetUp]
+    public async Task RunBeforeAnyTests()
+    {
+        _provider = new ServiceCollection()
+            .AddInfrastructureServices()
+            .AddMassTransitTestHarness(config =>
+            {
+                config.AddConsumers(Assembly.GetAssembly(typeof(IAppDbContext)));
+            })
+            .BuildServiceProvider(true);
+
+        var scope = _provider.CreateScope();
+
+        var initialiser = scope.ServiceProvider.GetRequiredService<AppDbContextInitialiser>();
+
+        await initialiser.TrySeedAsync();
+
+        Harness = _provider.GetRequiredService<ITestHarness>();
+
+        await Harness.Start();
+    }
+
+    [OneTimeTearDown]
+    public async Task RunAfterAnyTests()
+    {
+        _provider.Dispose();
+        await Harness.Stop();
+    }
+}
